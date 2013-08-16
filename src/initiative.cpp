b2b62f311aad9bc2a8973229fb95328b305aa48d
Hendrik v. Raven <hendrik@consetetur.de>
2013-08-16 16:34:52 +0200

    initiative: add conversion from string

--- src/initiative.cpp
+++ src/initiative.cpp
@@ -1,6 +1,7 @@
 #include "initiative.hpp"
 
 #include "dice.hpp"
+#include <regex>
 
 int
 initiative_c::roll()
@@ -8,3 +9,21 @@ initiative_c::roll()
   current = base + temp + roll_d6(dice);
   return current;
 };
+
+/**
+ * parses strings like 16+1d6 or 2d6+34
+ */
+bool
+initiative_c::set_initiative(const std::string& s)
+{
+  static const std::regex rgx{R"(\s*(\d+)\s*\+\s*(\d+)[dwDW]6)\s*)"};
+  std::smatch match;
+  if (std::regex_match(s, match, rgx))
+    {
+      set_base(std::stoi(match[1].str()));
+      set_dice(std::stoi(match[2].str()));
+      return true;
+    }
+  else
+    return false;
+}
--- src/initiative.hpp
+++ src/initiative.hpp
@@ -1,6 +1,8 @@
 #ifndef SHADOWRUN5_COMBAT_INITIATIVE_HPP
 #define SHADOWRUN5_COMBAT_INITIATIVE_HPP
 
+#include <string>
+
 class initiative_c
 {
 public:
@@ -14,6 +16,8 @@ public:
   void set_dice(int d) { dice = d; }
   void set_temp(int t) { temp = t; }
   void reset_temp() { temp = 0; }
+
+  bool set_initiative(const std::string&);
 
   initiative_c& operator+=(int n){ temp += n; return *this; }
 
